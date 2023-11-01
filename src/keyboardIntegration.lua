9964d51c89c76795c9f5ee2cf96d7c985219eb57
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-11-01 00:59:52 -0300

    feat: capturing keyboard input, string and number values

---
+++ src/keyboardIntegration.lua
@@ -0,0 +1,9 @@
+print("What is your name?")
+local name = io.read()
+print(name, type(name), "name")
+
+print('\n')
+
+print("What is your age?")
+local age = io.read("number")
+print(age, type(age), "age")
